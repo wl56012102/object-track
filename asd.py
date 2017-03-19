2a8a4c02065fa041e205ff59e49118df074577fb
madbob <735382689@qq.com>
2017-03-19 17:42:28 +0800

    add  get_point

---
+++ asd.py
@@ -0,0 +1,55 @@
+import cv2
+import dlib
+from skimage import io
+import glob
+import numpy as np
+
+camera = cv2.VideoCapture(0)
+pt1=[]
+pt2=[]
+global CATCH_FLAGS
+CATCH_FLAGS=False
+tracker = dlib.correlation_tracker()
+
+
+def selectroi(event,x,y,flags,param):
+    global CATCH_FLAGS
+    if event == cv2.EVENT_LBUTTONDOWN:
+        pt1.append((x,y))
+    elif event == cv2.EVENT_LBUTTONUP:
+        pt2.append((x,y))
+        CATCH_FLAGS =True
+
+
+
+print "Press key i to select "
+while True:
+    ret, frame = camera.read()
+    cv2.imshow("frame",frame)
+    cv2.setMouseCallback("frame", selectroi)
+    if cv2.waitKey(1) & 0xff == ord("i"):
+        break
+
+cv2.setMouseCallback("frame",selectroi)
+while CATCH_FLAGS is not True:
+    cv2.imshow("frame",frame)
+    cv2.waitKey(1) & 0xff
+cv2.destroyWindow("frame")
+img_select = frame[pt1[-1][1]:pt2[-1][1], pt1[-1][0]:pt2[-1][0]]
+img_select1 =np.array(img_select,dtype=np.uint8)
+
+
+while True:
+
+    cv2.imshow("img",img_select1)
+    dlib.rectangle
+    ret, frame = camera.read()
+    cv2.imshow("frame",frame)
+    #tracker.start_track(frame,img_select)
+
+
+    if cv2.waitKey(1) & 0xff ==ord("q"):
+        break
+cv2.destroyAllWindows()
+camera.release()
+
--- test.py
+++ test.py
@@ -1,5 +1,33 @@
-a=[]
-a.append((1,1))
-a.append((2,2))
+import dlib
+import cv2
 
-print a[-1]
+camera = cv2.VideoCapture(0)
+pt1=[]
+pt2=[]
+img_copy=0
+def drawcircle(event,x,y,flags,param):
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        pt1.append((x,y))
+    elif event == cv2.EVENT_LBUTTONUP:
+        pt2.append((x,y))
+        cv2.rectangle(frame,pt1[-1],pt2[-1],(0,255,255),3)
+        img_copy = frame[pt1[-1][1]:pt2[-1][1],pt1[-1][0]:pt2[-1][0]]
+        cv2.imshow("frame",img_copy)
+
+
+while True:
+    ret ,frame = camera.read()
+    
+    cv2.setMouseCallback("img",drawcircle)
+    if img_copy !=0:
+        cv2.imshow("img",img_copy)
+    print pt1,pt2
+    cv2.imshow("frame",frame)
+    k = cv2.waitKey(5) & 0xff
+
+    if k ==ord("q"):
+        break
+
+cv2.destroyAllWindows()
+camera.release()
